04c9aa095b22095843bf5f5fc2859ee0a8dfcb75
Le Hoang Trong <langtupro0456@gmail.com>
2024-06-04 21:08:34 +0700

    setup postman cho dự án

---
+++ src/NoteApp/Lecture.md
@@ -0,0 +1,19 @@
+# Project NextJS 14 - Xây dựng PosApp
+
+## Authentication, Authorization và Permission cho App
+
+### Cài đặt config ENV trong APP
+
+- biến môi trường ở browser thì browser hay server đều có thể thấy được
+
+- Trong khóa NextJS super này của chúng ta thì sẽ không set cookies ở đây
+
+### Setup Postman cho dự án
+
+### Cấu hình http cho dự án
+
+### Tạo Login route handler
+
+### Setup Tanstack Query và code logic login
+
+### Code logic logout
--- src/config.ts
+++ src/config.ts
@@ -5,10 +5,13 @@ const configSchema = z.object({
   NEXT_PUBLIC_URL: z.string()
 })
 
+// sẽ validate  cho chúng ta là các giá trị nó có đúng với schema hay không
 const configProject = configSchema.safeParse({
   NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
   NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL
 })
+
+// Nếu không đúng thì quăng ra lỗi
 if (!configProject.success) {
   console.error(configProject.error.issues)
   throw new Error('Các giá trị khai báo trong file .env không hợp lệ')
