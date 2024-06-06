0c29f4a0518f306353c24fb1890350ca06788b10
Le Hoang Trong <langtupro0456@gmail.com>
2024-06-06 16:41:04 +0700

    cài react query và thực hiện logic login

--- src/app/(public)/(auth)/login/login-form.tsx
+++ src/app/(public)/(auth)/login/login-form.tsx
@@ -7,8 +7,12 @@ import { useForm } from 'react-hook-form'
 import { Form, FormField, FormItem, FormMessage } from '@/components/ui/form'
 import { LoginBody, LoginBodyType } from '@/schemaValidations/auth.schema'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useLoginMutation } from '@/queries/useAuth'
+import { toast } from '@/components/ui/use-toast'
+import { handleErrorApi } from '@/lib/utils'
 
 export default function LoginForm() {
+  const loginMutation = useLoginMutation()
   const form = useForm<LoginBodyType>({
     resolver: zodResolver(LoginBody),
     defaultValues: {
@@ -16,6 +20,22 @@ export default function LoginForm() {
       password: ''
     }
   })
+
+  const onSubmit = async (data: LoginBodyType) => {
+    // Khi mà nhấn submit thì react hook form nó sẽ validate cái form bằng zode schema
+    if (loginMutation.isPending) return
+    try {
+      const result = await loginMutation.mutateAsync(data)
+      toast({
+        description: result.payload.message
+      })
+    } catch (error: any) {
+      handleErrorApi({
+        error,
+        setError: form.setError
+      })
+    }
+  }
 
   return (
     <Card className='mx-auto max-w-sm'>
@@ -25,7 +45,13 @@ export default function LoginForm() {
       </CardHeader>
       <CardContent>
         <Form {...form}>
-          <form className='space-y-2 max-w-[600px] flex-shrink-0 w-full' noValidate>
+          <form
+            onSubmit={form.handleSubmit(onSubmit, (err) => {
+              console.log('error', err)
+            })}
+            className='space-y-2 max-w-[600px] flex-shrink-0 w-full'
+            noValidate
+          >
             <div className='grid gap-4'>
               <FormField
                 control={form.control}
